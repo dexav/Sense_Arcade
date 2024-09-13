abd0df74a7591769d79017da1c3ced3e17eeba58
dexav <xnursiwat@gmail.com>
2024-09-13 10:29:56 +0200

    Highscore + Timer fixxed

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const catchCatBlock = document.querySelector('.catchCatBlock');
-    const reactionBlock = document.querySelector('.reactionBlock');
-    const catchCatGo = document.getElementById('catchCatGo');
-    const reactionGo = document.getElementById('reactionGo');
+document.addEventListener("DOMContentLoaded", () => {
+  const catchCatBlock = document.querySelector(".catchCatBlock");
+  const reactionBlock = document.querySelector(".reactionBlock");
+  const catchCatGo = document.getElementById("catchCatGo");
+  const reactionGo = document.getElementById("reactionGo");
+  const catchStart = document.getElementById("catchStart");
 
-    catchCatGo.addEventListener('click', () => {
-        catchCatBlock.style.display = 'flex';
-        reactionBlock.style.display = 'none';
-        document.querySelector('.selectGame').style.display = 'none';
-        startCatchGame();  // Startet das Fangspiel
-    });
+  catchCatGo.addEventListener("click", () => {
+    catchCatBlock.style.display = "flex";
+    reactionBlock.style.display = "none";
+    document.getElementById("catchCatImage").style.display = "none";
+    document.querySelector(".selectGame").style.display = "none";
+  });
 
-    reactionGo.addEventListener('click', () => {
-        reactionBlock.style.display = 'flex';
-        catchCatBlock.style.display = 'none';
-        document.querySelector('.selectGame').style.display = 'none';
-        startReactionGame();  // Funktion für das Reaktionsspiel
-    });
+  catchStart.addEventListener("click", () => {
+    catchCatBlock.style.display = "flex";
+    document.querySelector(".selectGame").style.display = "none";
+    document.getElementById("catchCatImage").style.display = "block";
+    startCatchGame(); // Funktion für das Fang
+  });
+
+  reactionGo.addEventListener("click", () => {
+    reactionBlock.style.display = "flex";
+    catchCatBlock.style.display = "none";
+    document.querySelector(".selectGame").style.display = "none";
+    startReactionGame(); // Funktion für das Reaktionsspiel
+  });
 });
 
-document.querySelectorAll('#catchBack button, #reactionBack button').forEach(button => {
-    button.addEventListener('click', () => {
-        document.querySelector('.catchCatBlock').style.display = 'none';
-        document.querySelector('.reactionBlock').style.display = 'none';
-        document.querySelector('.selectGame').style.display = 'flex';
+document
+  .querySelectorAll("#catchBack button, #reactionBack button")
+  .forEach((button) => {
+    button.addEventListener("click", () => {
+      document.querySelector(".catchCatBlock").style.display = "none";
+      document.querySelector(".reactionBlock").style.display = "none";
+      document.querySelector(".selectGame").style.display = "flex";
     });
-});
+  });
